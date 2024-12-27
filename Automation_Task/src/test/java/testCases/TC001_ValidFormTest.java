120174562d7b0004ee191e2b4ddc56be6021aa0c
2021t01252 <chamodlakshitha666@gmail.com>
2024-12-28 00:20:59 +0530

    commit3

--- Automation_Task/src/test/java/testCases/TC001_ValidFormTest.java
+++ Automation_Task/src/test/java/testCases/TC001_ValidFormTest.java
@@ -1,4 +1,32 @@
 package testCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TC001_ValidFormTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demoqa.com/automation-practice-form");
+    }
+
+    @Test
+    public void TC001(){
+
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
 }

2227042da6ba341553357275a7a33ad417c232b8
2021t01252 <chamodlakshitha666@gmail.com>
2024-12-28 01:42:24 +0530

    commit4

--- Automation_Task/src/main/java/pages/FormPage.java
+++ Automation_Task/src/main/java/pages/FormPage.java
@@ -1,4 +1,46 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class FormPage {
+
+    WebDriver driver;
+
+    public FormPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+
+    @FindBy(xpath = "//input[@id='firstName']")
+    WebElement firstName;
+
+    @FindBy(xpath = "//input[@id='lastName']")
+    WebElement lastName;
+
+    @FindBy(xpath = "//input[@id='userEmail']")
+    WebElement email;
+
+    @FindBy(xpath = "//input[@id='gender-radio-1']")
+    WebElement male;
+
+    @FindBy(xpath = "//input[@id='gender-radio-2']")
+    WebElement female;
+
+    @FindBy(xpath = "//input[@id='gender-radio-3']")
+    WebElement other;
+
+    @FindBy(xpath = "///input[@id='userNumber']")
+    WebElement mobileNumber;
+
+    @FindBy(xpath = "//input[@id='dateOfBirthInput']")
+    WebElement datePicker;
+
+    public void sendName(String name){
+
+        firstName.sendKeys(name);
+    }
 }
--- Automation_Task/src/test/java/testCases/TC001_ValidFormTest.java
+++ Automation_Task/src/test/java/testCases/TC001_ValidFormTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import pages.FormPage;
 import java.time.Duration;
 
 public class TC001_ValidFormTest {
@@ -22,6 +22,9 @@ public class TC001_ValidFormTest {
 
     @Test
     public void TC001(){
+
+        FormPage formPage = new FormPage(driver);
+        formPage.sendName("Chamod");
 
     }
 
